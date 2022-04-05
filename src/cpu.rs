9b9406f53147d824e720a941b5e0b865ae78504f
Esteban Borai <estebanborai@gmail.com>
2022-04-04 21:06:50 -0400

    feat: decode and execute instructions

--- src/cpu.rs
+++ src/cpu.rs
@@ -5,9 +5,82 @@ use crate::config::Config;
 use crate::memory::{Memory, USER_SPACE_STR};
 use crate::stack::Stack;
 
+/// Chip8 opcodes are 16-bit hexadecimal values which represent CPU
+/// instructions. These are decoded and interpreted accordingly based on the
+/// structure of the hexadecimal value.
+///
+/// # Anatomy of a Chip8 Opcode
+///
+/// The 16-bit value can be unpacked into 2 bytes, the High Byte and the
+/// Low Byte. These 2 bytes can also be represented as 4 nibbles (4-bit values),
+/// each byte would also contain a High Nibble and a Low Nibble.
+///
+/// ```ignore
+///      HB  LB
+///     _^_ _^_
+/// 0 x 1 2 C D
+///     | | | |_ Low nibble
+///     | | |___ High nibble
+///     | |_____ Low nibble
+///     |_______ High nibble
+///
+/// HB: High-Byte
+/// LB: Low-Byte
+/// ```
+///
+/// # Opcode Variables
+///
+/// An opcode can hold variables, based on the opcode purpose a byte or nibble
+/// could represent a value for the purpose of the instruction the opcode is
+/// intended to execute. For instance, the `0x1NNN` opcode holds the variable
+/// `NNN` which represents a memory address.
+///
+/// ```ignore
+/// | Variable  |                Position             |     Description    |
+/// |-----------|-------------------------------------|--------------------|
+/// | n         | Low Byte, Low Nibble                | Number of bytes    |
+/// | x         | High Byte, Low Nibble               | CPU Register       |
+/// | y         | Low Byte, High Nibble               | CPU Register       |
+/// | c         | High Byte, High Nibble              | Opcode Group       |
+/// | d         | Low Byte, Low Nibble                | Opcode Subgroup    |
+/// | kk        | Low Byte                            | Integer            |
+/// | nnn       | High Byte, Low Nibble and Low Byte  | Memory Address     |
+/// ```
+///
+/// Refer: https://en.wikipedia.org/wiki/CHIP-8#Opcode_table
 pub type Opcode = u16;
 
 pub type Rom = Vec<u8>;
+
+pub enum Instruction {
+    NoOp,
+    /// (`0x1NNN`) Sets the PC address to `NNN`
+    Jump(u16),
+    /// (`0x2NNN`) Calls subroutine at NNN.
+    CallSubroutine(u16),
+    /// (`0x3NNN`) Calls subroutine at NNN. `if (Vx == NN)`
+    CondEq(u8, u8),
+}
+
+impl Instruction {
+    pub fn execute(self, cpu: &mut Cpu) {
+        match self {
+            Instruction::Jump(address) => {
+                cpu.pc = address;
+            }
+            Instruction::CallSubroutine(_address) => {
+                todo!()
+            }
+            Instruction::CondEq(vx, kk) => {
+                println!("CondEq ==> Vx: {:#04x} kk: {:#04x}", vx, kk);
+                if vx == kk {
+                    cpu.pc += 2;
+                }
+            }
+            Instruction::NoOp => {}
+        }
+    }
+}
 
 pub struct Cpu {
     /// System available memory.
@@ -54,8 +127,9 @@ impl Cpu {
     /// the instruction and finally executes the instruction.
     pub fn cycle(&mut self) {
         let opcode = self.fetch_opcode();
+        let instruction = self.decode_opcode(opcode);
 
-        self.decode_opcode(opcode);
+        instruction.execute(self);
     }
 
     /// Fetches an OpCode from memory based on Program Counter (PC) and then
@@ -71,7 +145,7 @@ impl Cpu {
     ///
     /// 2. The value at memory address pointed by the PC + 1 is merged with
     /// the value created at step 1 using the OR operator.
-    fn fetch_opcode(&mut self) -> Opcode {
+    fn fetch_opcode(&mut self) -> u16 {
         let pc = self.pc as usize;
         let opcode: u16 = (self.memory[pc] as u16) << 8 | (self.memory[pc + 1] as u16);
 
@@ -80,20 +154,24 @@ impl Cpu {
     }
 
     /// Decodes the provided `OpCode` and executes it.
-    ///
-    /// Refer: https://en.wikipedia.org/wiki/CHIP-8#Opcode_table
-    fn decode_opcode(&mut self, opcode: Opcode) {
+    fn decode_opcode(&mut self, opcode: Opcode) -> Instruction {
         match opcode {
-            0x0000 => {}
+            0x0000 => Instruction::NoOp,
             // 00EE Flow Jumps to address NNN.
-            0x1000..=0x1FFF => self.jump(opcode),
+            0x1000..=0x1FFF => Instruction::Jump(opcode & 0x0FFF),
+            // 2NNN Flow *(0xNNN)() Calls subroutine at NNN.
+            0x2000..=0x2FFF => Instruction::CallSubroutine(opcode & 0x0FFF),
+            // 3XNN Cond if (Vx == NN) Skips the next instruction if VX
+            // equals NN. (Usually the next instruction is a jump to skip a
+            // code block);
+            0x3000..=0x3FFF => {
+                let x = ((opcode & 0x0F00) >> 8) as u8;
+                let kk = (opcode & 0x00FF) as u8;
+
+                Instruction::CondEq(x, kk)
+            }
             _ => panic!("OpCode: {:#04x} not supported", opcode),
         }
-    }
-
-    /// (`0x1NNN`) Sets the PC address to `NNN`
-    fn jump(&mut self, opcode: Opcode) {
-        self.pc = opcode & 0x0FFF;
     }
 }
 
@@ -125,5 +203,16 @@ mod tests {
         cpu.cycle();
 
         assert_eq!(cpu.pc, 0x2CD);
+    }
+
+    #[test]
+    fn support_opcode_condeq() {
+        let mut cpu = Cpu::new();
+        let rom = vec![0x32, 0x02];
+
+        cpu.load(&rom);
+        cpu.cycle();
+
+        assert_eq!(cpu.pc, 0x200 + 2);
     }
 }
